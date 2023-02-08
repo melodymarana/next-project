a0b4c51fb1281f09ace6649d7f64832227b4dcd8
melodymarana <melody.marana@gmail.com>
2023-02-08 22:50:02 +0700

    config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+/* const nextConfig = {
   reactStrictMode: true,
 }
 
 module.exports = nextConfig
+
+ */
+const isProd = process.env.NODE_ENV === 'production';
+module.exports = {
+  reactStrictMode: true,
+  assetPrefix: isProd ? '/next-project/' : '',
+};
